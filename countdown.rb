553de8e6cadd98611a529d1a9c66f9160fdc12ab
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-06 20:54:29 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,9 +2,9 @@
 
 def countdown(integer)
   timer = integer
-  while timer>0 do
-    timer -= 1
+  while timer > 0 do
     puts "#{timer} Second(s)!"
+      timer -= 1
   end
   "Happy New Year!"
 end
