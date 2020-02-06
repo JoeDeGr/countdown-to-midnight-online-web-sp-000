d52bf818dabcf7f03f63747a6aa4b7f25e35da88
Joseph DeGregorio <degregorio.joseph@gmail.com>
2020-02-06 20:56:43 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -6,5 +6,15 @@ def countdown(integer)
     puts "#{timer} Second(s)!"
       timer -= 1
   end
-  "Happy New Year!"
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(integer)
+  timer = integer
+  while timer > 0 do
+    puts "#{timer} Second(s)!"
+      timer -= 1
+      sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
